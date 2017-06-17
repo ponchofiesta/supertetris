8c99179c9847900ecf92e6ecc143e500a3da47d0
nosce <claudia.kutter@gmx.net>
2017-06-17 22:34:55 +0200

    Methods extended for multiplayer mode

--- src/main/java/de/vfh/pressanykey/supertetris/game/GameoverViewController.java
+++ src/main/java/de/vfh/pressanykey/supertetris/game/GameoverViewController.java
@@ -15,15 +15,27 @@ import java.util.ResourceBundle;
 public class GameoverViewController extends ViewController {
 
     private Scores scores;
+    private Scores oppScores;
 
     @FXML
     private Button btnHome;
+    @FXML
+    protected Label lblName;
     @FXML
     protected Label lblPoints2;
     @FXML
     protected Label lblLevel2;
     @FXML
     protected Label lblRows2;
+    @FXML
+    protected Label lblOppName;
+    @FXML
+    protected Label lblOppPoints;
+    @FXML
+    protected Label lblOppLevel;
+    @FXML
+    protected Label lblOppRows;
+
 
     public void btnHomeClick(ActionEvent actionEvent) throws Exception {
         setView((Stage)btnHome.getScene().getWindow(), "start.fxml");
@@ -35,6 +47,20 @@ public class GameoverViewController extends ViewController {
             lblPoints2.setText(scores.getPoints().toString());
             lblRows2.setText(scores.getLineCount().toString());
             lblLevel2.setText(scores.getLevel().toString());
+        }
+    }
+
+    public void setNames(String myName, String oppName) {
+        lblName.setText(myName);
+        lblOppName.setText(oppName);
+    }
+
+    public void setOppScores(Scores scores) {
+        this.oppScores = scores;
+        if(scores != null) {
+            lblOppPoints.setText(scores.getPoints().toString());
+            lblOppRows.setText(scores.getLineCount().toString());
+            lblOppLevel.setText(scores.getLevel().toString());
         }
     }
 }
