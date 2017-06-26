41eab5d26ab9fabbdd0361b103614a963fb90fe6
nerdbabe <mail@nerdbabe.de>
2017-06-26 21:48:09 +0200

    Play Music in the background

--- src/main/java/de/vfh/pressanykey/supertetris/SupertetrisApp.java
+++ src/main/java/de/vfh/pressanykey/supertetris/SupertetrisApp.java
@@ -1,20 +1,26 @@
 package de.vfh.pressanykey.supertetris;
 
 import javafx.application.Application;
+import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 
 /**
  * Main class
- * @author Michael Richter, Claudia Kutter
+ * @author Michael Richter, Claudia Kutter, Ute Mayer
  */
 public class SupertetrisApp extends Application {
 
     public static Stage primaryStage;
+    public static Task task;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -32,7 +38,7 @@ public class SupertetrisApp extends Application {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        startMusic();
     }
 
     public static Stage getPrimaryStage() {
@@ -41,5 +47,26 @@ public class SupertetrisApp extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    /**
+     * Play Tetris Music in a Background Thread
+     */
+    public static void startMusic() {
+        task = new Task() {
+            @Override
+            protected Object call() throws Exception {
+                ClassLoader classLoader = getClass().getClassLoader();
+                URL resource = classLoader.getResource("sounds/music.mp3");
+                Media sound = new Media(resource.toString());
+                MediaPlayer mediaPlayer = new MediaPlayer(sound);
+                mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+                mediaPlayer.play();
+                return null;
+            }
+        };
+
+        Thread thread = new Thread(task);
+        thread.start();
     }
 }
