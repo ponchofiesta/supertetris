250104c8fd9ee50c3951efa21657ab8548957f4c
Michael Richter <mr@osor.de>
2017-07-09 20:08:03 +0200

    removed Task/Thread from MusicPlayer

--- src/main/java/de/vfh/pressanykey/supertetris/game/MusicPlayer.java
+++ src/main/java/de/vfh/pressanykey/supertetris/game/MusicPlayer.java
@@ -1,6 +1,5 @@
 package de.vfh.pressanykey.supertetris.game;
 
-import javafx.concurrent.Task;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
@@ -12,26 +11,15 @@ import java.net.URL;
  * @author Ute Mayer
  */
 public class MusicPlayer {
-    private Task task;
-    private Thread thread;
     private static MusicPlayer instance;
 
     public void startMusic(String path, int times) {
-        task = new Task() {
-            @Override
-            protected Object call() throws Exception {
-                ClassLoader classLoader = getClass().getClassLoader();
-                URL resource = classLoader.getResource(path);
-                Media sound = new Media(resource.toString());
-                MediaPlayer mediaPlayer = new MediaPlayer(sound);
-                mediaPlayer.setCycleCount(times);
-                mediaPlayer.play();
-                return null;
-            }
-        };
-
-        thread = new Thread(task);
-        thread.start();
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(path);
+        Media sound = new Media(resource.toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(sound);
+        mediaPlayer.setCycleCount(times);
+        mediaPlayer.play();
     }
 
     public static MusicPlayer getInstance() {
